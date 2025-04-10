10fa83f66197b682f6ff4c8c32a0cdcf4095b507
Taishi1020 <tai.yakiman1020@gmail.com>
2025-04-10 18:12:07 +0900

    mongDB周りの接続設定

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const express = require("express");;
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express()
+
 const path = require("path");
+
+mongoose.connect("mongodb://localhost:27017/campground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {
+    console.log("データベースに成功");
+}).catch((error) => {
+    console.log("データベースに失敗");
+    console.log(error);
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
