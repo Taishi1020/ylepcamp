6082c41f49b005bbf92a42025f08552ac23e4585
Taishi1020 <tai.yakiman1020@gmail.com>
2025-04-11 16:08:27 +0900

    詳細画面の作成

--- app.js
+++ app.js
@@ -20,20 +20,24 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// ホーム画面
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-// キャンプ場の作成
-app.get("/makecampground", async (req, res) => {
-    // キャンプ場のデータベースのモデルを作成
-    const camp = new Campground({title: "私のキャンプ場", description: "これは私のキャンプ場です"});
-    // キャンプ場のデータベースのモデルを保存
-    await camp.save();
-    // キャンプ場のデータベースのモデルを表示
-    res.send(camp);
+// キャンプ場の一覧画面
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds});
 });
 
+// キャンプ場の詳細画面
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+});
+
+// ポート設定
 app.listen(3000, () => {
     console.log("ポート3000番で起動中");
 });
