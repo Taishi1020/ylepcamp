ae5578a5c8339a72b9b7b55012a412229136ad8c
Taishi1020 <tai.yakiman1020@gmail.com>
2025-04-15 22:41:09 +0900

    ejs-metaを導入

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://localhost:27017/campground", {
 const app = express()
 
 // テンプレートエンジンの設定(ミドルウェアの設定)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // expressに対して、フォームにデータを送信するときに、データを解析する
